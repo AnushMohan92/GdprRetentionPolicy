1297c9949263c182fe9f48e1513b81e4bc499d50
AnushMohan92 <anushsehwag99@gmail.com>
2022-03-15 23:20:29 +0530

    GDPR plugin code

--- GdprRetentionPolicy/UpdateAccountOnCaseClosure.cs
+++ GdprRetentionPolicy/UpdateAccountOnCaseClosure.cs
@@ -9,7 +9,11 @@ using Microsoft.Xrm.Sdk.Query;
 namespace GdprRetentionPolicy
 {
     public class UpdateAccountOnCaseClosure : IPlugin
-    {
+    {      
+          public const int ProblemSolved = 5;
+          public const int InformationProvided = 1000;
+          public const int Cancelled = 6;
+          public const int Merged = 2000;
         public void Execute(IServiceProvider serviceProvider)
         {
             IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
@@ -46,13 +50,14 @@ namespace GdprRetentionPolicy
 
                     if (accountAttributes.Attributes.Contains("tfl_numberofcasesclosed")){
                         casesClosed = accountAttributes.GetAttributeValue<int>("tfl_numberofcasesclosed");
-                    }                  
-
-                    if (accountRef != null && (statusCodeValue == 5 || statusCodeValue == 1000 || statusCodeValue == 6 || statusCodeValue == 2000))
+                    }        
+                   
+                    // Status reason codes for the case resolution & case cancellation
+                    if (accountRef != null && (statusCodeValue == ProblemSolved || statusCodeValue == InformationProvided || statusCodeValue == Cancelled || statusCodeValue == Merged))
                     {                        
                         Entity updateAccount = new Entity("account");
                         updateAccount.Id = accountRef.Id;
-                        updateAccount["tfl_retentiondate"] = modifiedOnDate.AddDays(1);
+                        updateAccount["tfl_retentiondate"] = modifiedOnDate.AddYears(3);
                         updateAccount["tfl_numberofcasesclosed"] = casesClosed + 1;
                         service.Update(updateAccount);
                         tracer.Trace("Account's date has been updated with the case resolution:" + statusCodeValue);
